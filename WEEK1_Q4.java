2aea65ad507f481210627f350494f89470eaed04
DakShH289 <singhdaksh535@gmail.com>
2026-08-22 19:39:03 +0530

    add code4

---
+++ WEEK1_Q4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class WEEK1_Q4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a CSV line: ");
+        String line = sc.nextLine();
+
+        String[] fields = line.split(",");
+
+        System.out.println("Fields:");
+
+        for (String field : fields) {
+            System.out.println(field.trim());
+        }
+
+        sc.close();
+    }
+}
