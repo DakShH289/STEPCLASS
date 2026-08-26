42f705ba1db4ccf54a3c04efaede80b5e64ae197
DakShH289 <singhdaksh535@gmail.com>
2026-08-26 10:21:59 +0530

    week1 q3

---
+++ WEEK1_Q3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SentenceReverser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split(" ");
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+
+            if (i != 0) {
+                reversed.append(" ");
+            }
+        }
+
+        System.out.println("Reversed sentence: " + reversed);
+
+        sc.close();
+    }
+}
